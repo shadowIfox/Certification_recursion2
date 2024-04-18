7192742082fd8218f7d92174518820849973a6a7
Ekaterina Mirovskaya <shadowicatsfox@yandex.ru>
2024-04-18 19:09:08 +0300

    Написала третью  задачу

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*
+Задание№3 Задайте произвольный массив. 
+Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.
+*/
+void OutpetElements (int[] array, int i = 0)
+{
+    if (i < array.Length)
+    {
+        OutpetElements(array, i + 1);
+        Console.Write(array[i] + " ");
+    }
+}
+int[] array = { 1, 2, 5, 0, 10, 34 };
+OutpetElements(array);
